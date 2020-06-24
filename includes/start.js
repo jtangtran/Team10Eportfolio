7efe9633900dbe531dc25c75382b546f043416d0
Jennifer Tang-Tran <phuongtangtran@gmail.com>
2020-06-23 20:18:39 -0700

    Added a hash function and changed the stylings

--- includes/start.js
+++ includes/start.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
-    //checking if the password is correct
+    //checking if the password is correct but first it hashes it 
+    //and then compares it with the correct password's hash
     //returns true if the password matches
     function passwordCheck() {
-        var password = "Team10/10";
-        return (($("#password").val() === password) ? true : false);
+        var password = $('#password').val();
+        var hash = stringToHash(password);
+        let hashedPassword = "1783682994"
+        return ((hash=== hashedPassword) ? true : false);
     }
+
+    //hashes the string from the parameters
+    //returns a hash value
+    function stringToHash(string) { 
+        var hash = 0; 
+        if (string.length == 0) return hash; 
+        for (i = 0; i < string.length; i++) { 
+            char = string.charCodeAt(i); 
+            hash = ((hash << 5) - hash) + char; 
+            hash = hash & hash; 
+        } 
+        return String(hash); 
+    } 
 
     //checks if the user presses the enter key to submit the form
     $("#password").keypress(function(e) {
@@ -21,12 +37,14 @@ $(document).ready(function() {
         var passwordResult = passwordCheck();
         if (passwordResult) {
             location.href = "index.html";
-            // $('#protected').hide();
-            // $('#navBar').fadeIn(500);
         }
         else {
+            //displays an error message and then reloads the page after 5 seconds
             $("#err").text("Incorrect Password");
-            location.reload();
+            //functions that refreshes the page in around 1 second
+            setTimeout(function(){
+                window.location.reload(1);
+             }, 1000);
         }  
     }); 
 });
