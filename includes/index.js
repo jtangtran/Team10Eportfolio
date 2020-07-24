7347aaa82b12fb545f466049cf1853b62314f00e
Jennifer Tang-Tran <phuongtangtran@gmail.com>
2020-07-24 09:54:49 -0700

    Updated the Draft after Katie's suggestions

--- includes/index.js
+++ includes/index.js
@@ -5,16 +5,5 @@ $(document).ready(function() {
         $("#footer").load("footer.html");
     });
 
-    var img = document.getElementById("thankYouGif");
-    document.getElementById('divText').innerHTML = img.alt;
-
-    var image = document.getElementById("logo");
-    document.getElementById('divLogo').innerHTML = image.alt;
-
-    var image = document.getElementById("camosunIcon");
-    document.getElementById('camosunAltText').innerHTML = image.alt;
-
-    var image = document.getElementById("landingPage");
-    document.getElementById('landPageAlt').innerHTML = image.alt;
 
 });
