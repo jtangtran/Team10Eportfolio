89efff6bfbe1c2f3d25e258f6c1f77c03646a453
Jennifer Tang-Tran <phuongtangtran@gmail.com>
2020-07-24 02:36:18 -0700

    Made a draft of the brochure

--- includes/index.js
+++ includes/index.js
@@ -2,5 +2,19 @@ $(document).ready(function() {
     $("#navBar").fadeIn(500);
     $(function(){
         $("#navBar-placeholder").load("navBar.html");
+        $("#footer").load("footer.html");
     });
+
+    var img = document.getElementById("thankYouGif");
+    document.getElementById('divText').innerHTML = img.alt;
+
+    var image = document.getElementById("logo");
+    document.getElementById('divLogo').innerHTML = image.alt;
+
+    var image = document.getElementById("camosunIcon");
+    document.getElementById('camosunAltText').innerHTML = image.alt;
+
+    var image = document.getElementById("landingPage");
+    document.getElementById('landPageAlt').innerHTML = image.alt;
+
 });
