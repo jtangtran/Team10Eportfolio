7a733350d306a75d89475c849b629f3e31ce9d62
Jennifer Tang-Tran <phuongtangtran@gmail.com>
2020-07-31 22:16:13 -0700

    Cleaned up the files

--- includes/document.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    //displays the nav bar from the index.html and footer.html file
-    $(function(){
-        $("#navBar-placeholder").load("navBar.html");
-        $("#footer").load("footer.html");
-    });
-});
--- includes/notes.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    //displays the nav bar from the index.html file
-    $(function(){
-        $("#navBar-placeholder").load("navBar.html");
-    });
-});
